8616de72e581edc719edbe039fb36cb32d686050
Kristoffer Linderman <kristoffer.linderman@knowit.se>
2020-11-19 16:06:22 +0100

    setup of project

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Title, MainSubTitle, Container } from './LandingPage.style';
+
+const LandingPage = () => (
+  <Container>
+    <Title>Hello, my name is Kristoffer Linderman</Title>
+    <MainSubTitle>
+      I write code and always want to become better.
+      Check out some of my projects, or just say hi by email or Linkedin.
+    </MainSubTitle>
+  </Container>
+);
+
+export default LandingPage;
---
+++ src/pages/LandingPage/LandingPage.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 2rem;
+`;
+
+const MainSubTitle = styled.p`
+  font-size: 1rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export { Title, MainSubTitle, Container };
