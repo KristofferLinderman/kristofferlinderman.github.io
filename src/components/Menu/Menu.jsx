bec7e1bb71b8b6d75f4494b2f8c60c38d00b576b
Kristoffer Linderman <kristoffer.linderman@knowit.se>
2020-11-24 15:07:33 +0100

    fixed scroll bug

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,23 +4,17 @@ import useScrollPosition from '../../hooks/useScrollPosition';
 import { SIZE } from '../../commonComponents/constants';
 import { MENU_STATES } from './contants';
 
-// https://dev.to/n8tb1t/tracking-scroll-position-with-react-hooks-3bbj For scroll
-
 const Menu = () => {
   const [menuState, setMenuState] = useState(MENU_STATES.TOP);
   const handlescroll = ({ currPos }) => {
-    // console.log('Previous: ', prevPos);
-    // console.log('Current: ', currPos);
-
-    if (currPos.y > window.innerHeight - SIZE.MENU && menuState !== MENU_STATES.STICKY) {
-      console.log('Get sticky!');
-      setMenuState(MENU_STATES.STICKY);
-    } else if (currPos.y > 0) {
-      console.log('Transition');
-      setMenuState(MENU_STATES.TRANSITION);
-    } else if (currPos.y === 0) {
-      console.log('Back');
+    if (currPos.y > window.innerHeight - SIZE.MENU) {
+      if (menuState !== MENU_STATES.STICKY) {
+        setMenuState(MENU_STATES.STICKY);
+      }
+    } else if (currPos.y < 10) {
       setMenuState(MENU_STATES.TOP);
+    } else if (menuState !== MENU_STATES.TRANSITION) {
+      setMenuState(MENU_STATES.TRANSITION);
     }
   };
 
